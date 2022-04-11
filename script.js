d75ce98edcb0003f51ef2f48141d6b745ce9047f
Aryanne Acosta <aryanneacosta@gmail.com>
2022-04-11 12:52:10 -0300

    requisitos cumpridos

--- script.js
+++ script.js
@@ -1,61 +1,66 @@
 let numeroCartas;
+let carta;
 let qtdCartas = [];
+let cartasEscolhidas = [];
+let cartasDiv = [];
+let cartasCertas = [];
+let jogadas = 0;
 
 let cartas = [
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="bobrossparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="bobrossparrot.gif" /></div>
     </div>`,
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="explodyparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="explodyparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="fiestaparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="fiestaparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="metalparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="metalparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="revertitparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="revertitparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="tripletsparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="tripletsparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="unicornparrot.gif" /></div>
     </div>`, 
-    `<div class="cards">
-            <div class="card front"><img src="front.png" /></div>
+    `<div class="cards" onclick="virandoCartas(this)">
+            <div class="card"><img src="front.png" /></div>
             <div class="card back"><img src="unicornparrot.gif" /></div>
     </div>`
 ];
@@ -84,7 +89,7 @@ function distribuicaoCartas() {
 function embaralharCartas() {
     qtdCartas.sort(comparador);
     for (let i = 0; i < numeroCartas; i++) {
-        let divCartas = document.querySelector(".game")
+        let divCartas = document.querySelector(".game");
         divCartas.innerHTML += qtdCartas[i];
     }
 }
@@ -93,9 +98,39 @@ function comparador() {
     return Math.random() - 0.5;
 }
 
-function virandoCarta() {
-    cartas = qtdCartas;
-    cartas.addEventListener('click', function () {
-        cartas.classList.add('flipped');
-    })
+function virandoCartas(carta) {
+        carta.querySelector(".card").classList.add("frontflip");
+        carta.querySelector(".back").classList.add("backflip"); 
+        cartasDiv.push(carta);
+        cartasEscolhidas.push(carta.querySelector(".back>img"));
+        
+        if (cartasEscolhidas.length === 2) {
+                setTimeout(verificarCartas, 1000)
+        }
+
+}
+
+
+
+function verificarCartas() {
+        if (cartasEscolhidas[0].src === cartasEscolhidas[1].src) {
+                cartasCertas.push(cartasEscolhidas);
+                
+        } else {
+                let cartaUm = cartasDiv[0];
+                let cartaDois = cartasDiv[1];
+                cartaUm.querySelector(".card").classList.remove("frontflip");
+                cartaUm.querySelector(".back").classList.remove("backflip"); 
+                cartaDois.querySelector(".card").classList.remove("frontflip");
+                cartaDois.querySelector(".back").classList.remove("backflip"); 
+        
+        }
+
+        cartasEscolhidas = [];
+        cartasDiv = [];
+        jogadas += 1;
+
+        if (cartasCertas.length === numeroCartas/2) {
+                alert(`Você ganhou em ${jogadas * 2} jogadas!`);
+        }
 }
